6ec5dee5d9d3e3430d5ffab8cab5efc90d07f35f
David <d888eightsix@icloud.com>
2023-09-22 14:58:39 +0700

    first commit

---
+++ q1.js
@@ -0,0 +1,15 @@
+function showRating(rating) {
+    let ratings = "";
+    for(let i = 0; i < Math.floor(rating); ++i) {
+        ratings = ratings + "*"
+        if(i !== Math.floor(rating) - 1) {
+            ratings = ratings + " "
+        }
+    }
+    if( !Number.isInteger(rating)){
+        ratings = ratings + "."
+    }
+    return ratings;
+}
+
+console.log(showRating(4.5));
---
+++ q2.js
@@ -0,0 +1,5 @@
+function sortLowToHigh(numbers) {
+    return numbers;
+}
+
+console.log( sortLowToHigh([1,5,0,10,4]));
